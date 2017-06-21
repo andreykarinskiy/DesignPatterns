f903645770787c40b182764b26a76be749b488e6
SPBHELIX\karinskiy.a <karinskiy.a@spb.helix.ru>
2017-06-21 18:17:54 +0300

    +

---
+++ Паттерны проектирования/Command/DragAndDrop.Wpf/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DragAndDrop.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly AppController appController;
+
+        public App()
+        {
+            appController = new AppController();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            appController.Run();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            appController.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ Паттерны проектирования/Command/DragAndDrop/AppController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragAndDrop
+{
+    public sealed class AppController : IDisposable
+    {
+        public void Run()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
