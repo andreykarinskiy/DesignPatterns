81897d355b9017b55a290ce053e3a6cbbad725ff
SPBHELIX\karinskiy.a <karinskiy.a@spb.helix.ru>
2017-07-04 16:37:27 +0300

    Singleton pattern

---
+++ Паттерны проектирования/Singleton/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Logger
+    {
+        private static Lazy<Logger> instance { get; } = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance => instance.Value;
+
+        public void Write(string message)
+        {
+            // Логика логгирования
+        }
+    }
+}
---
+++ Паттерны проектирования/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Logger.Instance.Write("Preved, Medved");
+
+        }
+    }
+}
