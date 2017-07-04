766d146d8abab7f47ca78fbda6b83f0081814bba
SPBHELIX\karinskiy.a <karinskiy.a@spb.helix.ru>
2017-07-04 14:03:15 +0300

    Adapter pattern

---
+++ Паттерны проектирования/Adapter/WeatherInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public sealed class WeatherInfo
+    {
+        public float Temperature { get; set; }
+
+        public float Humidity { get; set; }
+
+        public float Pressure { get; set; }
+    }
+}
