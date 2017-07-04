079f4332de4d70670ddfc99d44e4bf9ce57b0aa7
SPBHELIX\karinskiy.a <karinskiy.a@spb.helix.ru>
2017-07-04 16:12:47 +0300

    Prototype pattern

---
+++ Паттерны проектирования/Prototype/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public sealed class Employee : ICloneable
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Organization { set; get; }
+
+        public string WorkPhone { get; set; }
+
+        public string WorkAddress { get; set; }
+
+        public object Clone()
+        {
+            return new Employee
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Organization = Organization,
+                WorkAddress = WorkAddress,
+                WorkPhone = WorkPhone
+            };
+        }
+    }
+}
---
+++ Паттерны проектирования/Prototype/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person1 = new Employee
+            {
+                FirstName = "Василий",
+                LastName = "Пупкин",
+                Organization = "Рога и Копыта",
+                WorkPhone = "322223223322",
+                WorkAddress = "Красная Площадь"
+            };
+
+            var person2 = (Employee)person1.Clone();
+            person2.FirstName = "Абрам";
+            person2.LastName = "Шниперсон";
+        }
+    }
+}
