66dbc9768730168de2ec54d3847fcd4934245892
SPBHELIX\karinskiy.a <karinskiy.a@spb.helix.ru>
2017-07-04 16:01:30 +0300

    Iterator pattern

---
+++ Паттерны проектирования/Iterator/EnumerableExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Iterator
+{
+    public static class EnumerableExtension
+    {
+        public static IEnumerable<T> AsRandom<T>(this T[] source)
+        {
+            var indexes = GenerateRandomSequence(source.Length);
+
+            foreach (var i in indexes)
+            {
+                yield return source[i];
+            }
+        }
+
+        private static int[] GenerateRandomSequence(int total)
+        {
+            var random = new Random();
+
+            var sequence = new HashSet<int>();
+
+            while (sequence.Count < total)
+            {
+                sequence.Add(random.Next(0, total));
+            }
+
+            return sequence.ToArray();
+        }
+    }
+}
---
+++ Паттерны проектирования/Iterator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var str = new[] { "a", "b", "c", "d", "e", "f" };
+
+            foreach (var c in str.AsRandom())
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
